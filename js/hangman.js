717ebf617194f9a359e633072f7ac063a2cce172
xpy <cpiwarski01@gmail.com>
2019-07-05 10:59:28 -0700

    building

--- js/hangman.js
+++ js/hangman.js
@@ -5,6 +5,9 @@ var selectedHint = "";
 var board = [];
 var remainingGuesses = 6;
 var words = ["snake", "monkey", "beetle"];
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                'Y', 'Z']
 
 //LISTENERS
 window.onload = startGame()
@@ -24,17 +27,43 @@ function updateBoard() {
     }
 }
 
+//Creates the letter buttons for user input
+function createLetters() {
+    for (var letter of alphabet) {
+        $('#letters').append("<button> class='letter' id='" + letter + "'>" + letter + "</button>");
+    }
+}
+
+//Event handler for user letter input buttons
+$('.letter').click(function(){
+    console.log($(this).attr("id"));
+})
+
 //Selects a random word for a game of hangman
 function pickWord() {
     var randomInt = Math.floor(Math.random() * words.length);
-    selectedWord = words[randomInt];
+    selectedWord = words[randomInt].toUpperCase();
+}
+
+//Checks to see if the selected letter exists in the selectedWord
+function checkLetter(letter) {
+    var positions = new Array();
+
+    //Put all the positions the letter exists in an array
+    for (var i = 0; i < selectedWord.length; i++) {
+        console.log(selectedWord)
+        if (letter == selectedWord[i]) {
+            positions.push(i);
+        }
+    }
 }
 
 //Begins a game of hangman
 function startGame() {
-    pickWord()
-    initBoard()
-    updateBoard()
+    pickWord();
+    initBoard();
+    updateBoard();
+    createLetters();
 }
 
 
